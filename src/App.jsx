0286182584c90b9276edbae06577a2157623672b
Naspo <a.topaltsis@gmail.com>
2024-02-14 15:52:46 -0500

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import Menu from "./components/menu/Menu";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/footer/Footer";
+
+import "./css/App.css";
 
 function App() {
   return (
-    <>
-      <div className=""></div>
-    </>
+    <div className="App">
+      <Header />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/menu" element={Menu} />
+          <Route path="/contact" element={Contact} />
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+    </div>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,26 @@
-const Header = () => {
-  return <div>Header</div>;
-};
+import { Link } from "react-router-dom";
+import logo from "./src/assets/logo.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="inner-header">
+        <img src={logo} alt="Coffee Shop Logo" className="logo" />
+      </div>
+
+      <ul className="nav-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/menu">Menu</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
 
 export default Header;
