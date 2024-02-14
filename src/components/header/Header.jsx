c733e412d0c0ff29965593b68ce7a6848e8d0b60
Naspo <a.topaltsis@gmail.com>
2024-02-14 18:09:16 -0500

    Added font (Monserrat) from google fonts. Also added favicon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,24 +1,32 @@
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
+import "../../css/header.css";
 
 function Header() {
   return (
     <div className="header">
       <div className="inner-header">
-        <img src={logo} alt="Coffee Shop Logo" className="logo" />
+        <Link to="/" className="router-link">
+          <img src={logo} alt="Coffee Shop Logo" className="logo" />
+        </Link>
+        <ul className="nav-list">
+          <li>
+            <Link to="/" className="router-link">
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link to="/menu" className="router-link">
+              Menu
+            </Link>
+          </li>
+          <li>
+            <Link to="/contact" className="router-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
       </div>
-
-      <ul className="nav-list">
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/menu">Menu</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-      </ul>
     </div>
   );
 }
