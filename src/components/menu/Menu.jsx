0c8cbd62ac716993e7cba0da63b5412364d3709e
Naspo <a.topaltsis@gmail.com>
2024-02-15 16:29:24 -0500

    Started work on menu page. Also added new heading font

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,5 +1,28 @@
+import "../../css/menu.css";
+import * as menuItems from "./menu-items";
+
 function Menu() {
-  return <div>Menu</div>;
+  return (
+    <div className="menu">
+      <div className="heading-area">
+        <h1 className="title">Our Menu</h1>
+        <h3 className="subtitle">
+          Browse a variety of beautifully brewed coffees and homemade pastries.
+        </h3>
+      </div>
+
+      <div className="menu-list">
+        <div className="beverages-container menu-category-container">
+          <h2 className="menu-category-title">Beverages</h2>
+          <ul className="beverages-list menu-item-list">
+            {menuItems.beverages.map((beverage, index) => (
+              <li key={index}>{beverage}</li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Menu;
---
+++ src/components/menu/menu-items.js
@@ -0,0 +1,49 @@
+// Lists of menu items by category
+
+export const beverages = [
+  "Coffee (Regular, Decaf)",
+  "Espresso",
+  "Cappuccino",
+  "Latte",
+  "Mocha",
+  "Americano",
+  "Macchiato",
+  "Hot Chocolate",
+  "Tea (Various Flavors)",
+];
+
+export const pastries = [
+  "Croissants (Plain, Chocolate, Almond)",
+  "Muffins (Blueberry, Banana Nut, Chocolate Chip)",
+  "Scones (Cranberry Orange, Lemon Poppyseed)",
+  "Cinnamon Rolls",
+  "Danish Pastries (Cheese, Fruit)",
+  "Cookies (Chocolate Chip, Oatmeal Raisin, Sugar)",
+];
+
+export const breakfast = [
+  "Breakfast Sandwiches (Egg, Cheese, Bacon/Sausage on English Muffin/Biscuit)",
+  "Bagels (Plain, Everything) with Cream Cheese",
+  "Yogurt Parfait (Yogurt, Granola, Fresh Berries)",
+];
+
+export const lunch = [
+  "Soup of the Day",
+  "Sandwiches (Turkey & Swiss, Ham & Cheddar, Veggie)",
+  "Paninis (Chicken Pesto, Caprese, Turkey & Cranberry)",
+  "Salads (Caesar, Garden, Greek)",
+];
+
+export const desserts = [
+  "Cheesecake (Plain, Strawberry, Chocolate)",
+  "Tiramisu",
+  "Brownies",
+  "Pie (Apple, Cherry, Key Lime)",
+];
+
+export const specialtyDrinks = [
+  "Seasonal Specialty Drinks (Pumpkin Spice Latte, Peppermint Mocha)",
+  "Iced Coffee",
+  "Frappuccino",
+  "Smoothies (Fruit, Green)",
+];
