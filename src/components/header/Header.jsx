7dc6a790f9a53a3df141b91d2db26e58cc5f4aa4
Naspo <a.topaltsis@gmail.com>
2024-02-15 15:48:50 -0500

    Added header placeholder for more consistent top-of-page positioning

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,30 +4,41 @@ import "../../css/header.css";
 
 function Header() {
   return (
-    <div className="header">
-      <div className="inner-header">
-        <Link to="/" className="router-link">
-          <img src={logo} alt="Coffee Shop Logo" className="logo" />
-        </Link>
-        <ul className="nav-list">
-          <li>
-            <Link to="/" className="router-link">
-              Home
-            </Link>
-          </li>
-          <li>
-            <Link to="/menu" className="router-link">
-              Menu
-            </Link>
-          </li>
-          <li>
-            <Link to="/contact" className="router-link">
-              Contact
-            </Link>
-          </li>
-        </ul>
+    <>
+      <div className="header">
+        <div className="inner-header">
+          <Link to="/" className="router-link">
+            <img src={logo} alt="Coffee Shop Logo" className="logo" />
+          </Link>
+          <ul className="nav-list">
+            <li>
+              <Link to="/" className="router-link">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/menu" className="router-link">
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" className="router-link">
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-    </div>
+
+      {/* 
+    Header placeholder:
+    Used to maintain the space that the header would take up on the screen
+    if it wasn't position fixed.
+    
+    Makes starting positioning of content on pages below the header.
+    */}
+      <div className="header-placeholder"></div>
+    </>
   );
 }
 
