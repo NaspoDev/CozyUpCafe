2cd363850669b3d4daf06458ffc18f688930d5ef
Naspo <a.topaltsis@gmail.com>
2024-02-21 13:35:44 -0500

    Added a border to map

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -20,13 +20,15 @@ function Contact() {
       </div>
       <div className="content-area">
         <div className="embedded-map-container">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11510.070385378162!2d-79.4709185!3d43.845099499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2957d03f48a9%3A0xebb173891e4c5cab!2sStarbucks!5e0!3m2!1sen!2sca!4v1708229890129!5m2!1sen!2sca"
-            style={{ border: 0 }}
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
+          <div className="map-wrapper">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11510.070385378162!2d-79.4709185!3d43.845099499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2957d03f48a9%3A0xebb173891e4c5cab!2sStarbucks!5e0!3m2!1sen!2sca!4v1708229890129!5m2!1sen!2sca"
+              style={{ border: 0 }}
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
         </div>
 
         <div className="contact-info-container">
