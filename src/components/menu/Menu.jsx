7d567baf20885afe79ee4304ddae30085dc1c7d8
Naspo <a.topaltsis@gmail.com>
2024-02-21 14:41:39 -0500

    Started work on re-designing menu page

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -23,25 +23,36 @@ function Menu() {
       <div className="section-separator"></div>
 
       <div className="menu-list">
-        <div className="beverages-container menu-category-container">
-          <h2 className="menu-category-title">Beverages</h2>
-          <ul className="beverages-list menu-item-list">
-            {menuItems.beverages.map((beverage, index) => (
-              <li key={index}>{beverage}</li>
-            ))}
-          </ul>
-          <img src={coffee} alt="Coffee" className="coffee-image" />
+        {/* Menu section 1 */}
+        <div className="menu-section-1 menu-section">
+          <div className="left-side">
+            <div className="menu-paper-background">
+              <h2 className="menu-paper-heading">Cafe Essentials</h2>
+              <div className="beverages-container menu-category-container">
+                <h2 className="menu-category-title">Beverages</h2>
+                <ul className="beverages-list menu-item-list">
+                  {menuItems.beverages.map((beverage, index) => (
+                    <li key={index}>{beverage}</li>
+                  ))}
+                </ul>
+              </div>
+              <div className="pastries-container menu-category-container">
+                <h2 className="menu-category-title">Pastries</h2>
+                <ul className="pastries-list menu-item-list">
+                  {menuItems.pastries.map((pastry, index) => (
+                    <li key={index}>{pastry}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+
+          <div className="right-side">
+            <img src={coffee} alt="Coffee" className="coffee-image" />
+          </div>
         </div>
 
-        <div className="pastries-container menu-category-container">
-          <h2 className="menu-category-title">Pastries</h2>
-          <ul className="pastries-list menu-item-list">
-            {menuItems.pastries.map((pastry, index) => (
-              <li key={index}>{pastry}</li>
-            ))}
-          </ul>
-        </div>
-
+        {/* Large Image Divider */}
         <div className="large-image-container-1">
           <img
             src={assortedPastries}
@@ -50,45 +61,56 @@ function Menu() {
           />
         </div>
 
-        <div className="breakfast-container menu-category-container">
-          <h2 className="menu-category-title">Breakfast</h2>
-          <ul className="breakfast-list menu-item-list">
-            {menuItems.breakfast.map((breakfast, index) => (
-              <li key={index}>{breakfast}</li>
-            ))}
-          </ul>
-        </div>
+        {/* Menu Section 2 */}
+        <div className="menu-section-2 menu-section">
+          <div className="left-side">
+            <img
+              src={turkeySandwich}
+              alt="Turkey Sandwich"
+              className="turkey-sandwich-image"
+            />
+          </div>
+          <div className="right-side">
+            <div className="menu-paper-background">
+              <h2 className="menu-paper-heading">Food</h2>
+              <div className="breakfast-container menu-category-container">
+                <h2 className="menu-category-title">Breakfast</h2>
+                <ul className="breakfast-list menu-item-list">
+                  {menuItems.breakfast.map((breakfast, index) => (
+                    <li key={index}>{breakfast}</li>
+                  ))}
+                </ul>
+              </div>
 
-        <div className="lunch-container menu-category-container">
-          <h2 className="menu-category-title">Lunch</h2>
-          <ul className="lunch-list menu-item-list">
-            {menuItems.lunch.map((lunch, index) => (
-              <li key={index}>{lunch}</li>
-            ))}
-          </ul>
-          <img
-            src={turkeySandwich}
-            alt="Turkey Sandwich"
-            className="turkey-sandwich-image"
-          />
-        </div>
+              <h2 className="menu-paper-heading">Sweets & Specialties</h2>
+              <div className="lunch-container menu-category-container">
+                <h2 className="menu-category-title">Lunch</h2>
+                <ul className="lunch-list menu-item-list">
+                  {menuItems.lunch.map((lunch, index) => (
+                    <li key={index}>{lunch}</li>
+                  ))}
+                </ul>
+              </div>
 
-        <div className="desserts-container menu-category-container">
-          <h2 className="menu-category-title">Desserts</h2>
-          <ul className="desserts-list menu-item-list">
-            {menuItems.desserts.map((desserts, index) => (
-              <li key={index}>{desserts}</li>
-            ))}
-          </ul>
-        </div>
+              <div className="desserts-container menu-category-container">
+                <h2 className="menu-category-title">Desserts</h2>
+                <ul className="desserts-list menu-item-list">
+                  {menuItems.desserts.map((desserts, index) => (
+                    <li key={index}>{desserts}</li>
+                  ))}
+                </ul>
+              </div>
 
-        <div className="specialty-drinks-container menu-category-container">
-          <h2 className="menu-category-title">Specialty Drinks</h2>
-          <ul className="specialty-drinks-list menu-item-list">
-            {menuItems.specialtyDrinks.map((drink, index) => (
-              <li key={index}>{drink}</li>
-            ))}
-          </ul>
+              <div className="specialty-drinks-container menu-category-container">
+                <h2 className="menu-category-title">Specialty Drinks</h2>
+                <ul className="specialty-drinks-list menu-item-list">
+                  {menuItems.specialtyDrinks.map((drink, index) => (
+                    <li key={index}>{drink}</li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
