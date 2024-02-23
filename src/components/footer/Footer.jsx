78a40548519b401129e3bdaef876ec788f1e5109
Naspo <a.topaltsis@gmail.com>
2024-02-23 00:12:33 -0500

    Re-structured footer + added copyright and credit tag

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,11 +8,18 @@ function Footer() {
   return (
     <div className="footer">
       <div className="inner-footer">
-        <img src={logo} alt="Coffee Shop Logo" className="footer-logo" />
-
-        <div className="right-side-content">
+        <div className="footer-content">
+          <img src={logo} alt="Coffee Shop Logo" className="footer-logo" />
           <h3 className="company-name">CozyUpCafe</h3>
-          <div className="links-container">
+          <div className="right-side-content">
+            <div className="socials-container">
+              <a href="https://www.google.ca" target="_blank">
+                <img src={instaIcon} alt="Instagram Icon" />
+              </a>
+              <a href="https://www.google.ca" target="_blank">
+                <img src={fbIcon} alt="Facebook Icon" />
+              </a>
+            </div>
             <ul className="nav-list">
               <li>
                 <Link to="/" className="router-link">
@@ -30,16 +37,16 @@ function Footer() {
                 </Link>
               </li>
             </ul>
-            <div className="socials-container">
-              <a href="https://www.google.ca" target="_blank">
-                <img src={instaIcon} alt="Instagram Icon" />
-              </a>
-              <a href="https://www.google.ca" target="_blank">
-                <img src={fbIcon} alt="Facebook Icon" />
-              </a>
-            </div>
           </div>
         </div>
+
+        <p className="copyright-and-credits">
+          &copy; CozyUpCafe, 2024. Developed by{" "}
+          <a href="https://naspo.dev" className="naspodev-link" target="_blank">
+            Naspo
+          </a>
+          .
+        </p>
       </div>
     </div>
   );
