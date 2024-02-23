1ef837dbbc33f91930c79f423712539273af2dee
Naspo <a.topaltsis@gmail.com>
2024-02-23 15:23:25 -0500

    Changed embedded map fake location

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -22,7 +22,7 @@ function Contact() {
         <div className="embedded-map-container">
           <div className="map-wrapper">
             <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11510.070385378162!2d-79.4709185!3d43.845099499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2957d03f48a9%3A0xebb173891e4c5cab!2sStarbucks!5e0!3m2!1sen!2sca!4v1708229890129!5m2!1sen!2sca"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92290.46270098993!2d-79.45105466408205!3d43.70894969630176!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b34cffd7d2503%3A0xa4833e5e0100a6a6!2sStarbucks!5e0!3m2!1sen!2sca!4v1708719697301!5m2!1sen!2sca"
               style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
